ea98d1406b8a3b37a6500c36cebb96651b5ca222
shalkin228 <74145664+shalkin228@users.noreply.github.com>
2020-11-15 15:24:41 -0800

    Unity project

---
+++ FPS/Assets/chasing.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class chasing : MonoBehaviour
+{   private NavMeshAgent agent;
+    public GameObject player;
+    public float range;
+    public AnimationClip hit;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    void Update()
+    {   if(Vector3.Distance(player.transform.position,transform.position)< range)
+           agent.SetDestination(player.transform.position);
+        // (Vector3.Distance(player.transform.position, transform.position) == 1)
+            //hit.();
+
+    }
+}
